8b4f90e86973d1dbd317f8e80dcc455a7cd8d293
David <davidstutz@web.de>
2017-02-04 17:55:55 +0100

    Initial commit with README and sources.

---
+++ README.md
@@ -0,0 +1,44 @@
+# Example of Tensorflow Operation in C++
+
+This repository contains an example of a simple Tensorflow operation and its gradient both implemented in C++, as described in [this article](http://davidstutz.de/implementing-tensorflow-operations-in-c-including-gradients/).
+
+## Building
+
+The operation is built using [CMake](https://cmake.org/) and requires an appropriate version of Tensorflow to be installed. In order to get the necessary include directories containing the Tensorflow header files, the following trick is used (also see the [Tensorflow documentation](https://www.tensorflow.org/how_tos/adding_an_op/)):
+
+    import tensorflow
+    print(tensorflow.sysconfig.get_include())
+
+In the `CMakeLists.txt` this is used as follows:
+
+    execute_process(COMMAND python3 -c "import tensorflow; print(tensorflow.sysconfig.get_include())" OUTPUT_VARIABLE Tensorflow_INCLUDE_DIRS)
+
+The remaining contents are pretty standard. Building is now done using:
+
+    $ mkdir build
+    $ cd buils
+    $ cmake ..
+    $ make
+    Scanning dependencies of target inner_product
+    [ 50%] Building CXX object CMakeFiles/inner_product.dir/inner_product.cc.o
+    Linking CXX shared library libinner_product.so
+    [ 50%] Built target inner_product
+    Scanning dependencies of target inner_product_grad
+    [100%] Building CXX object CMakeFiles/inner_product_grad.dir/inner_product_grad.cc.o
+    Linking CXX shared library libinner_product_grad.so
+    [100%] Built target inner_product_grad
+
+`libinner_product.so` and `libinner_product_grad.so` can be found in `build` and need to be included in order to load the module in Python:
+
+    import tensorflow as tf
+    inner_product_module = tf.load_op_library('build/libinner_product.so')
+
+See `inner_product_tests.py` for usage examples.
+
+## License
+
+Copyright (c) 2016 David Stutz
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ inner_product_tests.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+"""
+Tests for the inner product Tensorflow operation.
+
+.. moduleauthor:: David Stutz
+"""
+
+import unittest
+import numpy as np
+import tensorflow as tf
+import _inner_product_grad
+inner_product_module = tf.load_op_library('build/libinner_product.so')
+
+class InnerProductOpTest(unittest.TestCase):
+    def test_raisesExceptionWithIncompatibleDimensions(self):
+        with tf.Session(''):
+            with self.assertRaises(ValueError):
+                inner_product_module.inner_product([1, 2], [[1, 2], [3, 4]]).eval()
+            with self.assertRaises(ValueError):
+                self.assertRaises(inner_product_module.inner_product([1, 2], [1, 2, 3, 4]).eval(), ValueError)
+            with self.assertRaises(ValueError):
+                self.assertRaises(inner_product_module.inner_product([1, 2, 3], [[1, 2], [3, 4]]).eval(), ValueError)
+            
+    def test_innerProductHardCoded(self):
+        with tf.Session(''):
+            result = inner_product_module.inner_product([[1], [2]], [[1, 2], [3, 4]]).eval()
+            self.assertEqual(result.shape[0], 2)
+            self.assertEqual(result[0], 5)
+            self.assertEqual(result[1], 11)
+    
+    def test_innerProductGradientXHardCoded(self):
+        with tf.Session('') as sess:
+            x = tf.placeholder(tf.float32, shape = (2))
+            W = tf.constant(np.asarray([[1, 2], [3, 4]]).astype(np.float32))
+            
+            Wx_tf = tf.matmul(W, tf.reshape(x, [-1, 1]))
+            Wx_inner_product = inner_product_module.inner_product(tf.reshape(x, [-1, 1]), W)
+            
+            grad_x_tf = tf.gradients(Wx_tf, x)
+            grad_x_inner_product = tf.gradients(Wx_inner_product, x)
+            
+            gradient_tf = sess.run(grad_x_tf, feed_dict = {x: np.asarray([1, 2]).astype(np.float32)})
+            gradient_inner_product = sess.run(grad_x_inner_product, feed_dict = {x: np.asarray([1, 2]).astype(np.float32)})
+            
+            self.assertEqual(gradient_tf[0][0], gradient_inner_product[0][0])
+            self.assertEqual(gradient_tf[0][1], gradient_inner_product[0][1])
+    
+    def test_innerProductGradientWHardCoded(self):
+        with tf.Session('') as sess:
+            x = tf.constant(np.asarray([1, 2]).astype(np.float32))
+            W = tf.placeholder(tf.float32, shape = (2, 2))
+            
+            Wx_tf = tf.matmul(W, tf.reshape(x, [-1, 1]))
+            Wx_inner_product = inner_product_module.inner_product(tf.reshape(x, [-1, 1]), W)
+            
+            grad_W_tf = tf.gradients(Wx_tf, W)
+            grad_W_inner_product = tf.gradients(Wx_inner_product, W)
+            
+            gradient_tf = sess.run(grad_W_tf, feed_dict = {W: np.asarray([[1, 2], [3, 4]]).astype(np.float32)})
+            gradient_inner_product = sess.run(grad_W_inner_product, feed_dict = {W: np.asarray([[1, 2], [3, 4]]).astype(np.float32)})
+            
+            self.assertEqual(gradient_tf[0][0][0], gradient_inner_product[0][0][0])
+            self.assertEqual(gradient_tf[0][0][1], gradient_inner_product[0][0][1])
+            self.assertEqual(gradient_tf[0][1][0], gradient_inner_product[0][1][0])
+            self.assertEqual(gradient_tf[0][1][1], gradient_inner_product[0][1][1])
+    
+    def test_innerProductRandom(self):
+        with tf.Session(''):
+            n = 4
+            m = 5
+            
+            for i in range(100):
+                x_rand = np.random.randint(10, size = (n, 1))
+                W_rand = np.random.randint(10, size = (m, n))
+                result_rand = np.dot(W_rand, x_rand)
+                
+                result = inner_product_module.inner_product(x_rand, W_rand).eval()
+                np.testing.assert_array_equal(result, result_rand)
+    
+    def test_innerProductGradientXRandom(self):
+        with tf.Session('') as sess:
+            n = 4
+            m = 5
+            
+            x = tf.placeholder(tf.float32, shape = (n))
+            W = tf.placeholder(tf.float32, shape = (m, n))
+            
+            Wx_tf = tf.matmul(W, tf.reshape(x, [-1, 1]))
+            Wx_inner_product = inner_product_module.inner_product(tf.reshape(x, [-1, 1]), W)
+            
+            grad_x_tf = tf.gradients(Wx_tf, x)
+            grad_x_inner_product = tf.gradients(Wx_inner_product, x)
+            
+            for i in range(100):
+                x_rand = np.random.randint(10, size = (n))
+                W_rand = np.random.randint(10, size = (m, n))
+                
+                gradient_tf = sess.run(grad_x_tf, feed_dict = {x: x_rand, W: W_rand})
+                gradient_inner_product = sess.run(grad_x_inner_product, feed_dict = {x: x_rand, W: W_rand})
+                
+                np.testing.assert_array_equal(gradient_tf, gradient_inner_product)
+                
+    def test_innerProductGradientWRandom(self):
+        with tf.Session('') as sess:
+            n = 4
+            m = 5
+            
+            x = tf.placeholder(tf.float32, shape = (n))
+            W = tf.placeholder(tf.float32, shape = (m, n))
+            
+            Wx_tf = tf.matmul(W, tf.reshape(x, [-1, 1]))
+            Wx_inner_product = inner_product_module.inner_product(tf.reshape(x, [-1, 1]), W)
+            
+            grad_W_tf = tf.gradients(Wx_tf, W)
+            grad_W_inner_product = tf.gradients(Wx_inner_product, W)
+            
+            for i in range(100):
+                x_rand = np.random.randint(10, size = (n))
+                W_rand = np.random.randint(10, size = (m, n))
+                
+                gradient_tf = sess.run(grad_W_tf, feed_dict = {x: x_rand, W: W_rand})
+                gradient_inner_product = sess.run(grad_W_inner_product, feed_dict = {x: x_rand, W: W_rand})
+                
+                np.testing.assert_array_equal(gradient_tf, gradient_inner_product)
+                  
+                
+if __name__ == '__main__':
+    unittest.main()
